010d5334536995961e71b73c46b52e34329a08f1
Rob Ranf <robranf@gmail.com>
2019-11-22 16:39:33 -0800

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import "/Users/rob/Developer/source/react-pics/src/style.css";
+
+const App = function () {
+    return (
+        <div className="ui container searchbar">
+            <SearchBar />
+        </div>
+    );
+};
+
+export default App;
